82122bad0527b17308787641f1302eb13773c9c0
Archie Boswell <archieboswell88@gmail.com>
2025-04-08 15:59:43 +0000

    Changed a join to a window function for simplicity

--- models/Refactoring_SQL_Course/Marts/Customer_orders.sql
+++ models/Refactoring_SQL_Course/Marts/Customer_orders.sql
@@ -30,99 +30,105 @@ payments as (
 
     select
 
-           orderid as order_id,
-            max(created) as payment_finalized_date, 
-            sum(amount) / 100.0 as total_amount_paid
+        orderid as order_id,
+        total_amount_paid,
+        payment_finalized_date,
     
-    
-     from {{ source('stripe_payments_refactor', 'payments') }}
-
-     where status <> 'fail' group by 1
-
-), 
-
-
+    * from {{ source('stripe_payments_refactor', 'payments') }}
+),
 
 
 --LOGICAL CTE
 
-paid_orders as 
+ complete_payments as (
 
-(  select 
-    orders.order_id,
-    orders.customer_id,
-    orders.order_placed_at,
-    orders.order_status,
-    payment.total_amount_paid,
-    payment.payment_finalized_date,
-    customers.customer_id as customer_id,
-    customers.customer_first_name,
-    customers.customer_last_name
+    select 
+        orderid as order_id,
+        max(created) as payment_finalized_date, 
+        sum(amount) / 100.0 as total_amount_paid
+    
+    from payments
+
+    where status <> 'fail'
+    
+    group by 1
+),
+
+    
+paid_orders as (
+
+    select 
+        orders.order_id,
+        orders.customer_id,
+        orders.order_placed_at,
+        orders.order_status,
+        complete_payments.total_amount_paid,
+        complete_payments.payment_finalized_date,
+        customers.customer_first_name,
+        customers.customer_last_name
 
     from orders
-    
-    left join  payments 
-        on orders.order_id = payment.order_id
 
-    left join customers 
-        on orders.customer_id = customers.customer_id 
+    left join  
+        complete_payments on orders.order_id = complete_payments.order_id
+
+    left join customers on orders.customer_id = customers.customer_id 
 
 ),
 
 customer_orders as (
     
     select 
-        
+        customers.customer_id as customer_id
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
         , count(orders.order_id) as number_of_orders
     from customers
 
     left join orders on orders.customer_id = customers.customer_id 
-    group by customers.customer_id)
+    group by 1)
 
 
+final as (
 
+    select
+        p.*,
 
+        --Sales transaction sequence
+        row_number() over 
+        (order by p.order_id) as transaction_seq,
 
+        --Customer Sales Sequence
+        row_number() over(
+        partition by customer_id 
+        order by p.order_id
+        ) as customer_sales_seq,
 
--- FINAL CTE
+        --New vs returning customer
+        case when customers.first_order_date = p.order_placed_at
+        then 'new'
+        else 'return' end as nvsr,
 
+        x.clv_bad as customer_lifetime_value,
 
+        --Customer lifetime value
+        sum(total_amount_paid) over (
+        partition by paid_orders.customer_id 
+        order by paid_orders.order_placed_at
+        ) as customer_lifetime_value,
 
+        customers.first_order_date as fdos
 
-
-
-
-
-select
-    p_ord.*,
-    row_number() over 
-    (order by p_ord.order_id) as transaction_seq,
-
-    row_number() over 
-    (partition by customer_id 
-    order by p_ord.order_id) as customer_sales_seq,
-
-    case when customers.first_order_date = p_ord.order_placed_at
-    then 'new'
-    else 'return' end as nvsr,
-
-    x.clv_bad as customer_lifetime_value,
-    customers.first_order_date as fdos
-from paid_orders p_ord
+    from paid_orders p
 
 left join customer_orders as c using (customer_id)
-left outer join 
-(
-    select
-            p_ord.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p_ord
-    
-    left join paid_orders t2 on p_ord.customer_id = t2.customer_id and p_ord.order_id >= t2.order_id
-    group by 1
-    order by p_ord.order_id
-) 
-    x on x.order_id = p_ord.order_id
+
+
+    x on x.order_id = p.order_id
     order by order_id
+    )
+
+
+    select * from final
+
+
