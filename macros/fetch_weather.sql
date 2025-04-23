4d77669b2ac68debb1d83ea4aacbdfba34df128d
Archie Boswell <archieboswell88@gmail.com>
2025-04-23 08:18:50 +0000

    Deleting all Lego and office_weather material

--- macros/fetch_weather.sql
+++
@@ -1,3 +0,0 @@
-{% macro fetch_weather() %}
-    {% do run_query("CALL DBT_COURSE.WEATHER.DBT_FETCH_WEATHER('"+ target.schema +"');")%}
-{% endmacro %}
--- models/office_weather/stg_weather_data.sql
+++
@@ -1,2 +0,0 @@
-select * 
-from {{source('office_weather','weather_readings')}}
