c188d3c8f374d2b46631a690a2af5740ad8dfe78
Archie Boswell <archieboswell88@gmail.com>
2025-04-23 09:20:55 +0000

    Removing all library loan work

--- models/library_loans/stg_loans.sql
+++
@@ -1,7 +0,0 @@
-SELECT *
-FROM {{ source("library", "loans") }}
-WHERE loan_id IS NOT NULL
-AND book_id IS NOT NULL
-AND member_id IS NOT NULL
-AND book_id IN (SELECT book_id FROM {{ ref('stg_books') }})
-AND member_id IN (SELECT member_id FROM {{ ref('stg_members') }})
--- models/library_loans/stg_members.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM {{ source("library", "members") }}
-WHERE member_id IS NOT NULL
-AND membership_tier IN ('Bronze', 'Silver', 'Gold')
